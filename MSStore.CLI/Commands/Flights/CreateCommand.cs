3c6d07f869ed8200ed7a19cb4646aa42092cf633
Alexandre Zollinger Chohfi <alzollin@microsoft.com>
2024-07-01 13:16:19 -0700

    Added create and delete flight commands.

--- MSStore.CLI/Commands/Flights/CreateCommand.cs
+++ MSStore.CLI/Commands/Flights/CreateCommand.cs
@@ -2,12 +2,15 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.ApplicationInsights;
 using Microsoft.Extensions.Logging;
+using MSStore.API;
 using MSStore.API.Models;
 using MSStore.CLI.Helpers;
 using MSStore.CLI.Services;
@@ -15,20 +18,37 @@ using Spectre.Console;
 
 namespace MSStore.CLI.Commands.Flights
 {
-    internal class GetCommand : Command
+    internal class CreateCommand : Command
     {
-        internal static readonly Argument<string> FlightIdArgument;
-
-        static GetCommand()
-        {
-            FlightIdArgument = new Argument<string>("flightId", "The flight Id.");
-        }
-
-        public GetCommand()
-            : base("get", "Retrieves a flight for the specified Application and flight.")
+        public CreateCommand()
+            : base("create", "Creates a flight for the specified Application and flight.")
         {
             AddArgument(SubmissionCommand.ProductIdArgument);
-            AddArgument(FlightIdArgument);
+
+            AddArgument(new Argument<string>("friendlyName", "The friendly name of the flight."));
+            var groupIdsOption = new Option<IEnumerable<string>>(
+                aliases: new string[]
+                {
+                    "--group-ids",
+                    "-g"
+                },
+                getDefaultValue: Array.Empty<string>,
+                description: "The group IDs to associate with the flight.")
+            {
+                AllowMultipleArgumentsPerToken = true
+            };
+            groupIdsOption.AddValidator((result) =>
+            {
+                var groupIds = result.Tokens.Select(t => t.Value).ToList();
+                if (groupIds.Count == 0)
+                {
+                    result.ErrorMessage = "At least one group ID must be provided.";
+                }
+            });
+
+            AddOption(groupIdsOption);
+
+            AddOption(new Option<string>(new[] { "--rank-higher-than", "-r" }, "The flight ID to rank higher than."));
         }
 
         public new class Handler : ICommandHandler
@@ -38,7 +58,9 @@ namespace MSStore.CLI.Commands.Flights
             private readonly TelemetryClient _telemetryClient;
 
             public string ProductId { get; set; } = null!;
-            public string FlightId { get; set; } = null!;
+            public string FriendlyName { get; set; } = null!;
+            public IEnumerable<string> GroupIds { get; set; } = null!;
+            public string? RankHigherThan { get; set; }
 
             public Handler(ILogger<Handler> logger, IStoreAPIFactory storeAPIFactory, TelemetryClient telemetryClient)
             {
@@ -63,23 +85,30 @@ namespace MSStore.CLI.Commands.Flights
                 }
 
                 return await _telemetryClient.TrackCommandEventAsync<Handler>(
-                    await AnsiConsole.Status().StartAsync("Retrieving Flight", async ctx =>
+                    await AnsiConsole.Status().StartAsync("Creating Flight", async ctx =>
                     {
                         try
                         {
                             var storePackagedAPI = await _storeAPIFactory.CreatePackagedAsync(ct: ct);
 
-                            var flight = await storePackagedAPI.GetFlightAsync(ProductId, FlightId, ct);
+                            var flight = await storePackagedAPI.CreateFlightAsync(ProductId, FriendlyName, GroupIds.ToList(), RankHigherThan, ct);
 
-                            ctx.SuccessStatus("[bold green]Retrieved Flight[/]");
+                            ctx.SuccessStatus("[bold green]Created Flight[/]");
 
                             AnsiConsole.WriteLine(JsonSerializer.Serialize(flight, SourceGenerationContext.GetCustom(true).DevCenterFlight));
                             return await _telemetryClient.TrackCommandEventAsync<Handler>(0, ct);
                         }
+                        catch (MSStoreHttpException err)
+                        {
+                            _logger.LogError(err, "Could not create the flight.");
+                            ctx.ErrorStatus("Could not create the flight.");
+
+                            return -1;
+                        }
                         catch (Exception err)
                         {
-                            _logger.LogError(err, "Error while retrieving Flight.");
-                            ctx.ErrorStatus(err);
+                            _logger.LogError(err, "Error while creating flight.");
+                            ctx.ErrorStatus("Error while creating flight. Please try again.");
                             return -1;
                         }
                     }), ct);
