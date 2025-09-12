439432c688ab535778fa1a9f21e7d4490f0d250e
Kiran Nangunuri <kinangun@microsoft.com>
2025-09-12 16:18:38 +0530

    Changed to update listings model to fix the listings update issue and honour all the fields

--- MSStore.API/Models/Listing.cs
+++ MSStore.API/Models/Listing.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 using System.Collections.Generic;
@@ -9,9 +9,15 @@ namespace MSStore.API.Models
     {
         public string? Language { get; set; }
         public string? Description { get; set; }
+        public string? WhatsNew { get; set; }
         public List<string>? ProductFeatures { get; set; }
+        public string? ShortDescription { get; set; }
         public List<string>? SearchTerms { get; set; }
         public string? AdditionalLicenseTerms { get; set; }
+        public string? Copyright { get; set; }
+        public string? DevelopedBy { get; set; }
         public List<SystemRequirementDetail>? Requirements { get; set; }
+        public string? SortTitle { get; set; }
+        public string? ContactInfo { get; set; }
     }
 }
